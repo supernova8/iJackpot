00b63a1e3ce5ba792868c1b4df294a9d8d642d38
Sonova Middleton <supernova8@Sonovas-MacBook-Pro.local>
2015-07-08 12:10:35 -0400

    Initial Commit

---
+++ iJackpot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iJackpot
+//
+//  Created by Sonova Middleton on 7/8/15.
+//  Copyright (c) 2015 supernova8productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
